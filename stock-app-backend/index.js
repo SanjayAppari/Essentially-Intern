a7178777f80fa1dfa12df356bda1d79043f45ea3
Appari Sai Sanjay <84316748+SanjayAppari@users.noreply.github.com>
2024-01-03 12:58:30 +0530

    Update index.js

--- stock-app-backend/index.js
+++ stock-app-backend/index.js
@@ -1,1 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const { fetchStocks, updateStockPrices, getStockPrices } = require('./stocksController.js');
 
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+
+fetchStocks();
+setInterval(updateStockPrices, 1000);
+
+app.get('/api/stocks', (req, res) => {
+  const stocks = getStockPrices();
+  res.json(stocks);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
